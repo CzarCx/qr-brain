0ee4eb1fba89243c0afe822e55a15d0c80a17a63
CésarG <emilians.gb@gmail.com>
2025-12-20 11:51:24 +0000

    sustituye estas credenciales: NEXT_PUBLIC_SUPABASE_URL="https://msnktspk

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,7 +1,8 @@
+
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://msnktspkrqloiyzzdlad.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1zbmt0c3BrcnFsb2l5enpkbGFkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYxNTM3MTAsImV4cCI6MjA3MTcyOTcxMH0.C1Vfl5A6DCrPLEyWvT1gEatWXWSdw2GjqQ2KiFBszPA'
+const supabaseUrl = 'https://zknhnivznhifhhpexipy.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InprbmhuaXZ6bmhpZmhocGV4aXB5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjYxOTYwODQsImV4cCI6MjA4MTc3MjA4NH0.upqkbcP8BQZhitKSkOpLRcGuwB4mwi9JcrlVWJUCpb8'
 
 if (!supabaseUrl) {
   throw new Error('Missing Supabase URL. Make sure it is set in your .env.local file');
