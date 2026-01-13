52e5f734cb20a479be5f283c6ec244a8f69b49ba
Paco Patricio <Frappesoluble@gmail.com>
2026-01-13 21:13:37 +0000

    quiero que ahora crees otro y todas las credenciales supabaseClient.ts l

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -2,19 +2,19 @@
 import { createClient } from '@supabase/supabase-js'
 
 // DB para personal, reportes, calificación, entrega
-const supabaseUrl = 'https://fjeffdiayxvbiteewgvz.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZqZWZmZGlheXh2Yml0ZWV3Z3Z6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIwMTkyOTcsImV4cCI6MjA3NzU5NTI5N30.xOC4_UjVZq2Zs2hnLeAbb694sF9GAMlGmrrgFVTdwKc'
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 // DB para consultar etiquetas_i
-const supabaseEtiquetasUrl = 'https://zknhnivznhifhhpexipy.supabase.co'
-const supabaseEtiquetasAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InprbmhuaXZ6bmhpZmhocGV4aXB5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjYxOTYwODQsImV4cCI6MjA4MTc3MjA4NH0.upqkbcP8BQZhitKSkOpLRcGuwB4mwi9JcrlVWJUCpb8'
+const supabaseEtiquetasUrl = process.env.NEXT_PUBLIC_SUPABASE_ETIQUETAS_URL;
+const supabaseEtiquetasAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ETIQUETAS_ANON_KEY;
 
 if (!supabaseUrl || !supabaseAnonKey) {
-  throw new Error('Faltan las credenciales de Supabase para la base de datos principal.');
+  throw new Error('Faltan las credenciales de Supabase para la base de datos principal en el archivo .env');
 }
 
 if (!supabaseEtiquetasUrl || !supabaseEtiquetasAnonKey) {
-  throw new Error('Faltan las credenciales de Supabase para la base de datos de etiquetas.');
+  throw new Error('Faltan las credenciales de Supabase para la base de datos de etiquetas en el archivo .env');
 }
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
